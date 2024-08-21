9b479aa358d5180e02ac849af7a3b561523dcea5
jun pasing <pasing_j@f112.lan>
2024-08-21 16:31:48 +0800

    2nd lab

--- randomcodegenerator/app.js
+++ randomcodegenerator/app.js
@@ -1,4 +1,4 @@
-const colors = [
+const quote = [
     "He who is brave is free. — Seneca", 
     "To be everywhere is to be nowhere. — Seneca", 
     "Don’t waste time on what you cannot control or change. — Meditations",
@@ -13,9 +13,9 @@ btn.addEventListener("click", () => {
 
     console.log(document.body)
     const randomNumber = getRandomNumber()
-    document.body.style.backgroundColor = colors[randomNumber]
-    color.textContent = colors [randomNumber]
+    document.body.style.backgroundColor = quote[randomNumber]
+    color.textContent = quote [randomNumber]
 }) 
 getRandomNumber = () => {
-    return Math.floor(Math.random() * colors.length)
+    return Math.floor(Math.random() * quote.length)
 }
