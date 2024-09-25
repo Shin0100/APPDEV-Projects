0acfafeb9fccc10901bc63c389597cb2ed381e2a
jun pasing <pasing_j@f112.lan>
2024-09-25 15:43:11 +0800

    REACT

---
+++ ColorPickerReact/src/Footer.jsx
@@ -0,0 +1,11 @@
+export default function(){
+    return(
+        <>
+        <footer>
+            <hr /><p>
+                &copy;{new Date().getFullYear()} My Color Picker Website || Written By: Jun Pasing
+            </p>
+        </footer>
+        </>
+    )
+}
---
+++ CounterReact/src/Footer.jsx
@@ -0,0 +1,11 @@
+export default function(){
+    return(
+        <>
+        <footer>
+            <hr /><p>
+                &copy;{new Date().getFullYear()} My Counter Website || Written By: Jun Pasing
+            </p>
+        </footer>
+        </>
+    )
+}
